bbeb228698ad5c40805dd1a873fdb8ee2ab550f9
woohojin <hojin0624@gmail.com>
2021-09-08 20:53:59 +0900

    update

---
+++ js/login.js
@@ -0,0 +1,8 @@
+const loginForm = document.querySelector("#login-form");
+loginId = loginForm.querySelector("#login-id");
+loginButton = loginForm.querySelector("#login-button");
+
+loginButton.onclick = function (event) {
+  if (event.target === loginRequire) {
+  }
+};
